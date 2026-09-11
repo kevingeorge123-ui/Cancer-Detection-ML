65c4fce4a96a98d7816c959e65f8babc3cc385d0
Kevin C George <kevincgeorge6084@gmail.com>
2026-09-11 14:37:29 -0500

    Update app title for breast cancer detection

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Kevin C George - Breast Cancer Detection")
+st.title("Breast Cancer Detection")
 
 st.write("Breast Cancer Detection using Machine Learning")
 
